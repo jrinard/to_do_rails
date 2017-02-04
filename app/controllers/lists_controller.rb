4bcae35d16dcfee55d4740e1b4a8026481eab5a8
joshua <jrinard@hotmail.com>
2017-02-03 19:19:07 -0800

    single list routing

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,12 @@
 class ListsController < ApplicationController
   def index
-    @lists = List.all
+    @lists = List.all   # shows all lists
     render :index
   end
+
+  def show
+    @list = List.find(params[:id]) # finds id of a single list
+    render :show
+  end
+
 end
