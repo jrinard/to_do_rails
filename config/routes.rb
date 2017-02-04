e736819326cb0fd1ac63947ec5d79ea068dddbb9
joshua <jrinard@hotmail.com>
2017-02-03 19:02:00 -0800

    lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lists  # sets up the 7 restful routs - create delete etc
 end
